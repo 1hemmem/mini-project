724c06ceee5165bbee8eac703b1328c41091f29f
BAHAEDDINE09 <charenbahaeddinehemmem@gmail.com>
2024-05-02 08:45:42 +0100

    cmmit

--- src/Places.jsx
+++ src/Places.jsx
@@ -1,3 +1,18 @@
-export const Places = () =>{
-    return(<section id="Top">Top places to visit/ resturants to eat in</section>)
-}
+import chrea from "./pictures/chrea.png";
+export const Places = () => {
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <img className="card-img-top pd" src={chrea} alt="Card image cap" style={{borderRadius: "20px"}}/>
+      <div class="card-body">
+        <h5 className="card-title">Chrea mountains</h5>
+        <p className="card-text">
+          Majestic peaks, tranquil lakes. Popular for hiking, nature
+          enthusiasts. Rich biodiversity.
+        </p>
+        <a href="#" class="btn btn-primary">
+          Read more
+        </a>
+      </div>
+    </div>
+  );
+};
